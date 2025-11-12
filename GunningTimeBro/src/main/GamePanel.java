d7fcdd2c0f8bf37b7484a69477c8db7cb5103536
Kiều Hoàng Quân <ITITIU24047@student.hcmiu.edu.vn>
2025-11-12 20:03:03 +0700

    Initial commit

---
+++ GunningTimeBro/src/main/GamePanel.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; // 16*16  tile. default
+    final int scale = 3; // 16*3 scale
+
+    public final int tileSize = originalTileSize * scale; // 48*48 tile // public cuz we use it in Player Class
+    public final int maxScreenCol = 20; // 4:3 window
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;  //48*20 = 960 pixels
+    public final int screenHeight = tileSize * maxScreenRow;  //48*12 = 576
+}
