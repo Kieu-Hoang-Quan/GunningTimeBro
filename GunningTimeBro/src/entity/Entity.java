db3e277194f56ed2104432ee7a24c58d9daaa99c
Kieu Hoang Quan <ITITIU24047@student.hcmiu.edu.vn>
2025-12-12 18:00:45 +0700

    Update Entity and remove compiled class files

--- GunningTimeBro/src/entity/Entity.java
+++ GunningTimeBro/src/entity/Entity.java
@@ -21,21 +21,11 @@ public abstract class Entity {
 
     }
 
-//    protected void drawHitbox(Graphics g) {
-//        // For debugging the hitbox
-////        g.setColor(Color.PINK);
-//        g.drawRect((int) hitbox.x, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
-//
-//    }
 
     protected void initHitbox(float x, float y, float width, float height) {
         hitbox = new Rectangle2D.Float(x, y, width, height);
     }
 
-//    protected void updateHitbox() {
-//        hitbox.x = (int) x;
-//        hitbox.y = (int) y;
-//    }
 
     public Rectangle2D.Float getHitbox() {
         return hitbox;
